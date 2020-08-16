7d052e17cde8d6f876fd0315981f01c68ba53b88
sachin <sachin.97@hotmail.com>
2020-08-16 12:17:24 -0400

    Implemented Initial Web Layout

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-/** Include Socket JS file - Back-end */
-var socket = require('./socket');
-
 var app = express();
 
 // view engine setup
